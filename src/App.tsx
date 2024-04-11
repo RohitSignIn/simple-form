4772194b84225a12e7fb62db1e39ccdb482549a4
RohitSignIn <rohitsinghsignin@gmail.com>
2024-04-11 11:44:48 +0530

    added sample env

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,36 @@ function App() {
         <div className='pt-[73px]'>
           <InfoForm />
         </div>
+        <div className='text-center'>
+          <p>@Bhagwan Singh</p>
+          <p className='p-2'>
+            <b>See All Entries:</b>{" "}
+            <a
+              href='https://contact-form-backend-x6uy.onrender.com/api/v1/contact'
+              target='_blank'
+            >
+              https://contact-form-backend-x6uy.onrender.com/api/v1/contact
+            </a>
+          </p>
+          <p className='p-2'>
+            <b>Github Repo Link Frontend:</b>{" "}
+            <a
+              href='https://github.com/RohitSignIn/simple-form'
+              target='_blank'
+            >
+              https://github.com/RohitSignIn/simple-form
+            </a>
+          </p>
+          <p className='p-2'>
+            <b>Github Repo Link Backend:</b>{" "}
+            <a
+              href='https://github.com/RohitSignIn/contact-form-backend'
+              target='_blank'
+            >
+              https://github.com/RohitSignIn/contact-form-backend
+            </a>
+          </p>
+        </div>
       </div>
     </>
   );
